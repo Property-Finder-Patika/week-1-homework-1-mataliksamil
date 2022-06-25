33305910bcb06788e4130e4a768f0f0f8369617e
Şamil Atalık <92802998+mataliksamil@users.noreply.github.com>
2022-06-25 23:52:26 +0300

    end of 3rd lesson

--- week1/lesson3/example1/greet.go
+++ week1/lesson3/example1/greet.go
@@ -6,12 +6,16 @@ type Person struct {
 	name string
 }
 
-func (p Person) greet() string {
-	return "Selam " + p.name + " :) "
+// this declaration means when I call greet func p instance will be available
+
+func (p Person) greet(q Person) string {
+	return "Selam from " + q.name + " to " + p.name + " :) "
 }
 
 func main() {
 	var greeter Person = Person{name: "Nihal"}
-	greeting := greeter.greet()
+	var myFriend Person = Person{name: "Mehmet"}
+	// now we call greet funciton with greeter object and passing myFriend
+	greeting := greeter.greet(myFriend)
 	fmt.Printf("%s \n", greeting)
 }
